2ac6ef5335e66efa842f3395d50099536af78700
Hussien2605 <168656898+Hussien2605@users.noreply.github.com>
2024-09-05 15:24:17 +0300

    Add files via upload

--- FrontEnd/ui_setup.py
+++ FrontEnd/ui_setup.py
@@ -6,7 +6,7 @@ def setup_ui(window, find_password, generate_password, save_data):
     window.config(padx=50, pady=50)
 
     canvas = Canvas(height=200, width=200)
-    logo_img = PhotoImage(file="FrontEnd/logo.png")  # Ensure this path is correct
+    logo_img = PhotoImage(file="FrontEnd/logo.png")
     canvas.create_image(100, 100, image=logo_img)
     canvas.grid(row=0, column=1)
 
